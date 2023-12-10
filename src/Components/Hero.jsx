59ffee74c08a5d2fc362576b9d6a292dd6ce6665
Demian <demyan.korotchenko@gmail.com>
2023-12-10 17:53:25 +0100

    123

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>Welcome to Our Car Rental Service</h1>
+      <p>Explore our amazing collection of rental cars.</p>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './Hero';
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      {/* Додайте інші компоненти для сторінки Home */}
+    </div>
+  );
+};
+
+export default Home;
